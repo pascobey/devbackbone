213401aff481772df5e100c3dac592b58e179f4a
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2020-12-11 22:43:59 -0600

    adding host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "backbone_bdd_production"
-
+  config.action_mailer.default_url_options = { :host => 'pacific-citadel-61301.herokuapp.com' }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
