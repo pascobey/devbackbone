2569d026c6f0341db78ff3c8cf792a72f59773ec
Parker Scobey <parkerscobey@Parkers-Air.attlocal.net>
2021-01-07 20:29:05 -0600

    fixing this mail fiasco

--- app/controllers/stickies_controller.rb
+++ app/controllers/stickies_controller.rb
@@ -3,10 +3,8 @@ class StickiesController < ApplicationController
   def move
     sticky = Sticky.find(params[:id])
     sticky.insert_at(params[:position].to_i)
-    if params[:category_id]
-      sticky.update(category_id: params[:category_id].to_s)
-      Entry.create(change_log_id: ChangeLog.find_by(project_id: params[:id]).id, committer_id: current_user.id, message: "#{Profile.find_by(user_id: current_user.id).user_information_safe['first_name']} moved a user story into #{Category.find(params[:category_id]).name}.", type_meta: 'backlog')
-    end
+    sticky.update(category_id: params[:category_id].to_s)
+    Entry.create(change_log_id: ChangeLog.find_by(project_id: params[:id]).id, committer_id: current_user.id, message: "#{Profile.find_by(user_id: current_user.id).user_information_safe['first_name']} moved a user story into #{Category.find(params[:category_id]).name}.", type_meta: 'backlog')
     head :ok
   end
 
